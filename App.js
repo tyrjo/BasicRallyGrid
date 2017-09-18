df9369a4059f5e7822b09404e00eba0d2bbbd009
Tyr Johanson <tyr@ca.com>
2017-09-18 16:15:05 -0600

    refactored

--- App.js
+++ App.js
@@ -2,24 +2,31 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     launch: function() {
-        var myStore = Ext.create('Rally.data.wsapi.Store', {
+        this._createStore();
+    },
+
+    _createStore: function() {
+        Ext.create('Rally.data.wsapi.Store', {
             model: 'User Story',
             autoLoad: true,
             listeners: {
                 load: function(store, data, success) {
-                    console.log("got data", data);
-                    var myGrid = Ext.create('Rally.ui.grid.Grid', {
-                        store: store,
-                        columnCfgs: [
-                            'FormattedID', 'Name', 'ScheduleState'
-                        ],
-                        //renderTo: Ext.getBody(),
-                    });
-                    this.add(myGrid);
+                    this._createGrid(store);
                 },
                 scope: this
             },
             fetch: ['FormattedId', 'Name', 'ScheduleState']
         });
+    },
+
+    _createGrid: function (store) {
+        var myGrid = Ext.create('Rally.ui.grid.Grid', {
+            store: store,
+            columnCfgs: [
+                'FormattedID', 'Name', 'ScheduleState'
+            ],
+            //renderTo: Ext.getBody(),
+        });
+        this.add(myGrid);
     }
 });
